Fixed all Syntax Errors and filled in res of seq2seq code. Added Adaptive Softmax

data.py
@@ -10,13 +10,29 @@ from tqdm import tqdm
 from collections import Counter
 import collections
 
+import mmap
+
+def get_num_lines(file_path):
+    fp = open(file_path, "r+")
+    buf = mmap.mmap(fp.fileno(), 0)
+    lines = 0
+    while buf.readline():
+        lines += 1
+    return lines
+
 SOS_WORD = '<SOS>'
 EOS_WORD = '<EOS>'
 PAD_WORD = '<PAD>'
 UNK_WORD = '<UNK>'
 
+def dd():
+    return defaultdict(int)
+
+def dd3():
+    return 3
+
 class Vocab():
-    def __init__(max_vocab=100000):
+    def __init__(self, max_vocab=60000):
         self.stoi = {}
         self.itos = []
         self.itos.append(SOS_WORD)
@@ -25,194 +41,209 @@ class Vocab():
         self.itos.append(UNK_WORD)
         self.max_vocab = max_vocab
 
+
     def create_vocab_for_translation(self, all_translation_files_path):
         v = []
-        f = open(all_translation_files_path, all_translation_files_path, 'r')
+        f = open(all_translation_files_path, 'r')
         for lines in f:
             lines = lines.split()
             v.extend(lines)
         v = Counter(v).most_common(self.max_vocab)
-        v = [w for w in v[0]]
+        # print(v)
+        v = [w[0] for w in v]
         self.itos.extend(v)
-        self.stoi = collections.defaultdict(lambda:3, {v:k for k,v in enumerate(self.itos)})
+        self.stoi = collections.defaultdict(dd3, {v:k for k,v in enumerate(self.itos)})
 
 
 
 class Preprocess():
-    def __init__(self, train_src_file, train_tgt_file, val_file, vocab_generation_file=None):
+    def __init__(self, train_src_file, train_tgt_file, val_src_file, val_tgt_file, vocab_generation_file, max_len=50):
+        # self.vocab = vocab
         self.train_src_file = train_src_file
         self.train_tgt_file = train_tgt_file
+        self.val_src_file = val_src_file
+        self.val_tgt_file = val_tgt_file
+        self.max_len = max_len
+
         self.lang2idx = {'en':0, 'fr':1, 'de':2}
 
         self.vocab = Vocab()
-        if vocab_generation_file is not None:
-            self.vocab.create_vocab_for_translation(vocab_generation_file)
+        self.vocab.create_vocab_for_translation(vocab_generation_file)
+
         self.train_src = []
         self.train_tgt = []
+        self.val_src = []
+        self.val_tgt = []
         self.train_srclng = []
         self.train_tgtlng = []
+        self.val_srclng = []
+        self.val_tgtlng = []
 
 
 
     def preprocess_train(self):
         with open(self.train_src_file) as fsrc, open(self.train_tgt_file) as ftgt:
-            for src_line, tgt_line in tqdm(zip(fsrc, ftgt)):
+            for src_line, tgt_line in tqdm(zip(fsrc, ftgt), total=get_num_lines(self.train_src_file)):
                 src_line, tgt_line = src_line.split(), tgt_line.split()
-                src_seq = [vocab.stoi[w] for w in src_line]
-                tgt_seq = [vocab.stoi[w] for w in tgt_line]
+                src_line = src_line + ['<EOS>']
+                tgt_line = tgt_line + ['<EOS>']
+                src_seq = [self.vocab.stoi[w] for w in src_line][:self.max_len]
+                tgt_seq = [self.vocab.stoi[w] for w in tgt_line][:self.max_len]
 
-                train_src.append(src_seq)
-                train_tgt.append(tgt_seq)
+                self.train_src.append(src_seq)
+                self.train_tgt.append(tgt_seq)
 
                 self.train_srclng.append(self.lang2idx[src_line[0]])
                 self.train_tgtlng.append(self.lang2idx[tgt_line[0]])
 
     def preprocess_val(self):
         with open(self.val_src_file) as fsrc, open(self.val_tgt_file) as ftgt:
-            for src_line, tgt_line in tqdm(zip(fsrc, ftgt)):
+            for src_line, tgt_line in tqdm(zip(fsrc, ftgt), total=get_num_lines(self.val_src_file)):
                 src_line, tgt_line = src_line.split(), tgt_line.split()
-                src_seq = [vocab.stoi[w] for w in src_line]
-                tgt_seq = [vocab.stoi[w] for w in tgt_line]
+                src_line = src_line + ['<EOS>']
+                tgt_line = tgt_line + ['<EOS>']
+                src_seq = [self.vocab.stoi[w] for w in src_line][:self.max_len]
+                tgt_seq = [self.vocab.stoi[w] for w in tgt_line][:self.max_len]
 
-                train_src.append(src_seq)
-                train_tgt.append(tgt_seq)
+                self.val_src.append(src_seq)
+                self.val_tgt.append(tgt_seq)
 
-                self.train_srclng.append(self.lang2idx[src_line[0]])
-                self.train_tgtlng.append(self.lang2idx[tgt_line[0]])
-
-
-
-
-class MaxlenTranslationDataset(data.Dataset):
-	# Code modified from
-	# https://github.com/pytorch/text/blob/master/torchtext/datasets/translation.py
-	# to be able to control the max length of the source and target sentences
-
-    def __init__(self, path, exts, fields, max_len=None, **kwargs):
-
-        if not isinstance(fields[0], (tuple, list)):
-            fields = [('src', fields[0]), ('trg', fields[1])]
-
-        src_path, trg_path = tuple(os.path.expanduser(path + x) for x in exts)
-
-        examples = []
-        with open(src_path) as src_file, open(trg_path) as trg_file:
-            for src_line, trg_line in tqdm(zip(src_file, trg_file)):
-                src_line, trg_line = src_line.split(' '), trg_line.split(' ')
-                if max_len is not None:
-                	src_line = src_line[:max_len]
-                    src_line = src_line + exts[0].split('.')[1]
-                	src_line = str(' '.join(src_line))
-                	trg_line = trg_line[:max_len]
-                    trg_line = trg_line.insert(exts[1].split('.')[1], 0)
-                	trg_line = str(' '.join(trg_line))
-
-                if src_line != '' and trg_line != '':
-                    examples.append(data.Example.fromlist(
-                        [src_line, trg_line], fields))
-
-        super(MaxlenTranslationDataset, self).__init__(examples, fields, **kwargs)
+                self.val_srclng.append(self.lang2idx[src_line[0]])
+                self.val_tgtlng.append(self.lang2idx[tgt_line[0]])
 
 
-class DataPreprocessor(object):
-	def __init__(self):
-		self.src_field, self.trg_field = self.generate_fields()
 
-	def preprocess(self, train_path, val_path, train_file, val_file, src_lang, trg_lang, max_len=None):
-		# Generating torchtext dataset class
-		print ("Preprocessing vocab dataset...")
-		train_dataset = self.generate_data(train_path, src_lang, trg_lang, max_len)
-
-		print ("Saving train dataset...")
-		self.save_data(train_file, train_dataset)
-
-		print ("Preprocessing validation dataset...")
-		val_dataset = self.generate_data(val_path, src_lang, trg_lang, max_len)
-
-		print ("Saving validation dataset...")
-		self.save_data(val_file, val_dataset)
-
-		# Building field vocabulary
-		self.src_field.build_vocab(train_dataset, max_size=30000)
-		self.trg_field.build_vocab(train_dataset, max_size=30000)
-
-		src_vocab, trg_vocab, src_inv_vocab, trg_inv_vocab = self.generate_vocabs()
-
-		vocabs = {'src_vocab': src_vocab, 'trg_vocab':trg_vocab,
-			  'src_inv_vocab':src_inv_vocab, 'trg_inv_vocab':trg_inv_vocab}
-
-		return train_dataset, val_dataset, vocabs
-
-	def load_data(self, vocab_generation_file, train_file, val_file):
-
-		# Loading saved data
-        vocab_dataset = torch.load(vocab_generation_file)
-		vocab_examples = vocab_dataset['examples']
-
-        train_dataset = torch.load(train_file)
-		train_examples = train_dataset['examples']
-
-		val_dataset = torch.load(val_file)
-		val_examples = val_dataset['examples']
-
-		# Generating torchtext dataset class
-		fields = [('src', self.src_field), ('trg', self.trg_field)]
-
-        vocab_dataset = data.Dataset(fields=fields, examples=train_examples)
-        train_dataset = data.Dataset(fields=fields, examples=train_examples)
-		val_dataset = data.Dataset(fields=fields, examples=val_examples)
-
-		# Building field vocabulary
-		self.src_field.build_vocab(train_dataset, max_size=30000)
-		self.trg_field.build_vocab(train_dataset, max_size=30000)
-
-		src_vocab, trg_vocab, src_inv_vocab, trg_inv_vocab = self.generate_vocabs()
-		vocabs = {'src_vocab': src_vocab, 'trg_vocab':trg_vocab,
-			  'src_inv_vocab':src_inv_vocab, 'trg_inv_vocab':trg_inv_vocab}
-
-		return train_dataset, val_dataset, vocabs
-
-
-	def save_data(self, data_file, dataset):
-
-		examples = vars(dataset)['examples']
-		dataset = {'examples': examples}
-
-		torch.save(dataset, data_file)
-
-	def generate_fields(self):
-	    src_field = data.Field(tokenize=data.get_tokenizer('spacy'),
-	                           eos_token=EOS_WORD,
-	                           pad_token=PAD_WORD,
-	                           include_lengths=True,
-	                           batch_first=True)
-
-	    trg_field = data.Field(tokenize=data.get_tokenizer('spacy'),
-	                           eos_token=EOS_WORD,
-	                           pad_token=PAD_WORD,
-	                           include_lengths=True,
-	                           batch_first=True)
-
-	    return src_field, trg_field
-
-	def generate_data(self, data_path, src_lang, trg_lang, max_len=None):
-	    exts = ('.'+src_lang, '.'+trg_lang)
-
-	    dataset = MaxlenTranslationDataset(
-	        path=data_path,
-	        exts=(exts),
-	        fields=(self.src_field, self.trg_field),
-	        max_len=max_len)
-
-	    return dataset
-
-	def generate_vocabs(self):
-	    # Define string to index vocabs
-	    src_vocab = self.src_field.vocab.stoi
-	    trg_vocab = self.trg_field.vocab.stoi
-
-	    # Define index to string vocabs
-	    src_inv_vocab = self.src_field.vocab.itos
-	    trg_inv_vocab = self.trg_field.vocab.itos
-
-	    return src_vocab, trg_vocab, src_inv_vocab, trg_inv_vocab
+#
+# class MaxlenTranslationDataset(data.Dataset):
+# 	# Code modified from
+# 	# https://github.com/pytorch/text/blob/master/torchtext/datasets/translation.py
+# 	# to be able to control the max length of the source and target sentences
+#
+#     def __init__(self, path, exts, fields, max_len=None, **kwargs):
+#
+#         if not isinstance(fields[0], (tuple, list)):
+#             fields = [('src', fields[0]), ('trg', fields[1])]
+#
+#         src_path, trg_path = tuple(os.path.expanduser(path + x) for x in exts)
+#
+#         examples = []
+#         with open(src_path) as src_file, open(trg_path) as trg_file:
+#             for src_line, trg_line in tqdm(zip(src_file, trg_file)):
+#                 src_line, trg_line = src_line.split(' '), trg_line.split(' ')
+#                 if max_len is not None:
+#                 	src_line = src_line[:max_len]
+#                     src_line = src_line + exts[0].split('.')[1]
+#                 	src_line = str(' '.join(src_line))
+#                 	trg_line = trg_line[:max_len]
+#                     trg_line = trg_line.insert(exts[1].split('.')[1], 0)
+#                 	trg_line = str(' '.join(trg_line))
+#
+#                 if src_line != '' and trg_line != '':
+#                     examples.append(data.Example.fromlist(
+#                         [src_line, trg_line], fields))
+#
+#         super(MaxlenTranslationDataset, self).__init__(examples, fields, **kwargs)
+#
+#
+# class DataPreprocessor(object):
+# 	def __init__(self):
+# 		self.src_field, self.trg_field = self.generate_fields()
+#
+# 	def preprocess(self, train_path, val_path, train_file, val_file, src_lang, trg_lang, max_len=None):
+# 		# Generating torchtext dataset class
+# 		print ("Preprocessing vocab dataset...")
+# 		train_dataset = self.generate_data(train_path, src_lang, trg_lang, max_len)
+#
+# 		print ("Saving train dataset...")
+# 		self.save_data(train_file, train_dataset)
+#
+# 		print ("Preprocessing validation dataset...")
+# 		val_dataset = self.generate_data(val_path, src_lang, trg_lang, max_len)
+#
+# 		print ("Saving validation dataset...")
+# 		self.save_data(val_file, val_dataset)
+#
+# 		# Building field vocabulary
+# 		self.src_field.build_vocab(train_dataset, max_size=30000)
+# 		self.trg_field.build_vocab(train_dataset, max_size=30000)
+#
+# 		src_vocab, trg_vocab, src_inv_vocab, trg_inv_vocab = self.generate_vocabs()
+#
+# 		vocabs = {'src_vocab': src_vocab, 'trg_vocab':trg_vocab,
+# 			  'src_inv_vocab':src_inv_vocab, 'trg_inv_vocab':trg_inv_vocab}
+#
+# 		return train_dataset, val_dataset, vocabs
+#
+# 	def load_data(self, vocab_generation_file, train_file, val_file):
+#
+# 		# Loading saved data
+#         vocab_dataset = torch.load(vocab_generation_file)
+# 		vocab_examples = vocab_dataset['examples']
+#
+#         train_dataset = torch.load(train_file)
+# 		train_examples = train_dataset['examples']
+#
+# 		val_dataset = torch.load(val_file)
+# 		val_examples = val_dataset['examples']
+#
+# 		# Generating torchtext dataset class
+# 		fields = [('src', self.src_field), ('trg', self.trg_field)]
+#
+#         vocab_dataset = data.Dataset(fields=fields, examples=train_examples)
+#         train_dataset = data.Dataset(fields=fields, examples=train_examples)
+# 		val_dataset = data.Dataset(fields=fields, examples=val_examples)
+#
+# 		# Building field vocabulary
+# 		self.src_field.build_vocab(train_dataset, max_size=30000)
+# 		self.trg_field.build_vocab(train_dataset, max_size=30000)
+#
+# 		src_vocab, trg_vocab, src_inv_vocab, trg_inv_vocab = self.generate_vocabs()
+# 		vocabs = {'src_vocab': src_vocab, 'trg_vocab':trg_vocab,
+# 			  'src_inv_vocab':src_inv_vocab, 'trg_inv_vocab':trg_inv_vocab}
+#
+# 		return train_dataset, val_dataset, vocabs
+#
+#
+# 	def save_data(self, data_file, dataset):
+#
+# 		examples = vars(dataset)['examples']
+# 		dataset = {'examples': examples}
+#
+# 		torch.save(dataset, data_file)
+#
+# 	def generate_fields(self):
+# 	    src_field = data.Field(tokenize=data.get_tokenizer('spacy'),
+# 	                           eos_token=EOS_WORD,
+# 	                           pad_token=PAD_WORD,
+# 	                           include_lengths=True,
+# 	                           batch_first=True)
+#
+# 	    trg_field = data.Field(tokenize=data.get_tokenizer('spacy'),
+# 	                           eos_token=EOS_WORD,
+# 	                           pad_token=PAD_WORD,
+# 	                           include_lengths=True,
+# 	                           batch_first=True)
+#
+# 	    return src_field, trg_field
+#
+# 	def generate_data(self, data_path, src_lang, trg_lang, max_len=None):
+# 	    exts = ('.'+src_lang, '.'+trg_lang)
+#
+# 	    dataset = MaxlenTranslationDataset(
+# 	        path=data_path,
+# 	        exts=(exts),
+# 	        fields=(self.src_field, self.trg_field),
+# 	        max_len=max_len)
+#
+# 	    return dataset
+#
+# 	def generate_vocabs(self):
+# 	    # Define string to index vocabs
+# 	    src_vocab = self.src_field.vocab.stoi
+# 	    trg_vocab = self.trg_field.vocab.stoi
+#
+# 	    # Define index to string vocabs
+# 	    src_inv_vocab = self.src_field.vocab.itos
+# 	    trg_inv_vocab = self.trg_field.vocab.itos
+#
+# 	    return src_vocab, trg_vocab, src_inv_vocab, trg_inv_vocab

main.py
@@ -3,48 +3,50 @@ import os
 import time
 from torch.backends import cudnn
 
-from prepro import *
+from data import *
 from trainer import *
-
+import _pickle as pickle
 
 def main(args):
 	cuda.set_device(int(args.gpu_num))
 	cudnn.benchmark = True
 
-	# Load dataset
-	vocab_generation_file = os.path.join(args.data_path + 'train_all', "data_{}_{}_{}_{}.json".format(args.dataset + '_vocab', args.src_lang,
-										args.trg_lang, args.max_len))
-	train_file = os.path.join(args.data_path, "data_{}_{}_{}_{}.json".format(args.dataset, args.src_lang,
-										args.trg_lang, args.max_len))
-	val_file = os.path.join(args.data_path, "data_dev_{}_{}_{}.json".format(args.src_lang, args.trg_lang, args.max_len))
-
 	start_time = time.time()
-	if os.path.isfile(vocab_generation_file) and os.path.isfile(train_file) and os.path.isfile(val_file):
-		print ("Loading data..")
-		dp = DataPreprocessor()
-		vocab_dataset, train_dataset, val_dataset, vocabs = dp.load_data(vocab_generation_file, train_file, val_file)
+
+	# if os.path.isfile(args.data_path + 'vocab.pb'):
+	# 	vocab = pickle.load(open(args.data_path, 'rb'))
+	# else:
+	# vocab = Vocab()
+	# vocab.create_vocab_for_translation(args.train_path + 'train_all')
+	# pickle.dump(vocab, open(args.data_path + 'vocab.pb', 'wb'))
+
+
+	# Load dataset
+	if os.path.isfile(args.data_path + 'prepro.pb'):
+		dp = pickle.load(open(args.data_path + 'prepro.pb', 'rb'))
 	else:
-		print ("Preprocessing data..")
-		dp = DataPreprocessor()
-		train_dataset, val_dataset, vocabs = dp.preprocess(args.train_path, args.train_path, args.val_path, vocab_generation_file, train_file, val_file,
-								   args.src_lang, args.trg_lang, args.max_len)
+		dp = Preprocess(args.train_path + 'train.src', args.train_path + 'train.tgt', args.val_path + 'val.src', args.val_path + 'val.tgt', args.train_path + 'train_all')
+		dp.preprocess_train()
+		dp.preprocess_val()
+		pickle.dump(dp, open(args.data_path + 'prepro.pb', 'wb'))
 
 
 	print ("Elapsed Time: %1.3f \n"  %(time.time() - start_time))
 
+	training_dataset = list(zip(dp.train_src, dp.train_tgt, dp.train_srclng, dp.train_tgtlng))
+	import random; random.seed(1234);
+	random.shuffle(training_dataset)
+	MAX_SAMPLES  = 400000
+	dp.train_src, dp.src_tgt, dp.train_srclng, dp.train_tgtlng = zip(*training_dataset)
+	dp.train_src, dp.src_tgt, dp.train_srclng, dp.train_tgtlng = dp.train_src[:MAX_SAMPLES], dp.src_tgt[:MAX_SAMPLES], dp.train_srclng[:MAX_SAMPLES], dp.train_tgtlng[:MAX_SAMPLES]
+
 	print ("=========== Data Stat ===========")
-	print ("Train: ", len(train_dataset))
-	print ("val: ", len(val_dataset))
+	print ("Train: ", len(dp.train_src))
+	print ("val: ", len(dp.val_tgt))
 	print ("=================================")
 
-	train_loader = data.BucketIterator(dataset=train_dataset, batch_size=args.batch_size,
-	                                   repeat=False, shuffle=True, sort_within_batch=True,
-	                                   sort_key=lambda x: len(x.src))
-	val_loader = data.BucketIterator(dataset=val_dataset, batch_size=args.batch_size,
-	                                 repeat=False, shuffle=True, sort_within_batch=True,
-	                                 sort_key=lambda x: len(x.src))
 
-	trainer = Trainer(train_loader, val_loader, vocabs, args)
+	trainer = Trainer(dp, args)
 	trainer.train()
 
 
@@ -58,12 +60,16 @@ if __name__ == '__main__':
 	                    help='list of type of lexical relations to capture. Options | syn | hyp | mer')
 	parser.add_argument('--trg_lang', type=str, default='en')
 	parser.add_argument('--max_len', type=int, default=50)
-
+	parser.add_argument('--cuda', action='store_true',
+	                    help='use CUDA')
+	parser.add_argument('--gpu', type=int, default=0,
+	                    help='use gpu x')
+	parser.add_argument('--log-interval', type=int, default=100)
 	# Model hyper-parameters
-	parser.add_argument('--lr', type=float, default=0.0001)
+	parser.add_argument('--lr', type=float, default=0.001)
 	parser.add_argument('--grad_clip', type=float, default=2)
-	parser.add_argument('--num_layer', type=int, default=2)
-	parser.add_argument('--embed_dim',  type=int, default=512)
+	parser.add_argument('--num_layer', type=int, default=1)
+	parser.add_argument('--embed_dim',  type=int, default=300)
 	parser.add_argument('--hidden_dim', type=int, default=1024)
 
 	# Training setting
@@ -71,7 +77,7 @@ if __name__ == '__main__':
 	parser.add_argument('--num_epoch', type=int, default=100)
 
 	# Path
-	parser.add_argument('--data_path', type=str, default='./data/training/')
+	parser.add_argument('--data_path', type=str, default='./data/')
 	parser.add_argument('--train_path', type=str, default='./data/training/')
 	parser.add_argument('--val_path', type=str, default='./data/dev/')
 

model/Decoder.py
@@ -7,89 +7,56 @@ from utils import *
 from model import *
 
 class Decoder(nn.Module):
-    def __init__(self, vocab_size, embed_dim, hidden_dim, max_len, trg_soi):
-        super(Decoder, self).__init__()
-        self.hidden_dim = hidden_dim
-        self.max_len = max_len
-        self.vocab_size = vocab_size
-        self.trg_soi = trg_soi
-        
-        self.embed = nn.Embedding(vocab_size, embed_dim)        
-        self.attention = Attention(hidden_dim) 
-        self.decodercell = DecoderCell(embed_dim, hidden_dim)
-        self.dec2word = nn.Linear(hidden_dim, vocab_size)
+    def __init__(self, output_dim, emb_dim, hid_dim, n_layers, dropout=0.5):
+        super().__init__()
 
+        self.emb_dim = emb_dim
+        self.hid_dim = hid_dim
+        self.output_dim = output_dim
+        self.n_layers = n_layers
+        self.dropout = dropout
 
-    def forward(self, enc_h, prev_s, target=None):
-        '''
-        enc_h  : B x S x 2*H 
-        prev_s : B x H
-        '''
+        self.embedding = nn.Embedding(output_dim, emb_dim)
 
-        if target is not None:
-            batch_size, target_len = target.size(0), target.size(1)
-            
-            dec_h = Variable(torch.zeros(batch_size, target_len, self.hidden_dim))
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
 
-            if torch.cuda.is_available():
-                dec_h = dec_h.cuda()
+        self.out = nn.Linear(hid_dim, output_dim)
 
-            target = self.embed(target)  
-            for i in range(target_len):
-                ctx = self.attention(enc_h, prev_s)                     
-                prev_s = self.decodercell(target[:, i], prev_s, ctx)       
-                dec_h[:,i,:] = prev_s.unsqueeze(1)
+        self.adaptive_softmax = nn.AdaptiveLogSoftmaxWithLoss(hid_dim, output_dim, cutoffs=[1000,10000])
 
-            outputs = self.dec2word(dec_h)
+        self.dropout = nn.Dropout(dropout)
 
-        else:
-            batch_size = enc_h.size(0)
-            target = Variable(torch.LongTensor([self.trg_soi] * batch_size), volatile=True).view(batch_size, 1)
-            outputs = Variable(torch.zeros(batch_size, self.max_len, self.vocab_size))
+    def forward(self, input, hidden, cell):
 
-            if torch.cuda.is_available():
-                target = target.cuda()
-                outputs = outputs.cuda()
-            
-            for i in range(self.max_len):
-                target = self.embed(target).squeeze(1)              
-                ctx = self.attention(enc_h, prev_s)                 
-                prev_s = self.decodercell(target, prev_s, ctx)
-                output = self.dec2word(prev_s) 
-                outputs[:,i,:] = output
-                target = output.topk(1)[1]
-            
-        return outputs
+        #input = [batch size]
+        #hidden = [n layers * n directions, batch size, hid dim]
+        #cell = [n layers * n directions, batch size, hid dim]
 
+        #n directions in the decoder will both always be 1, therefore:
+        #hidden = [n layers, batch size, hid dim]
+        #context = [n layers, batch size, hid dim]
 
-class DecoderCell(nn.Module):
-    def __init__(self, embed_dim, hidden_dim):
-        super(DecoderCell, self).__init__()
+        input = input.unsqueeze(0)
 
-        self.input_weights = nn.Linear(embed_dim, hidden_dim*2)
-        self.hidden_weights = nn.Linear(hidden_dim, hidden_dim*2)
-        self.ctx_weights = nn.Linear(hidden_dim*2, hidden_dim*2)
+        #input = [1, batch size]
 
-        self.input_in = nn.Linear(embed_dim, hidden_dim)
-        self.hidden_in = nn.Linear(hidden_dim, hidden_dim)
-        self.ctx_in = nn.Linear(hidden_dim*2, hidden_dim)
+        embedded = self.dropout(self.embedding(input))
 
+        #embedded = [1, batch size, emb dim]
 
-    def forward(self, trg_word, prev_s, ctx):
-        '''
-        trg_word : B x E
-        prev_s   : B x H 
-        ctx      : B x 2*H
-        '''
-        gates = self.input_weights(trg_word) + self.hidden_weights(prev_s) + self.ctx_weights(ctx)
-        reset_gate, update_gate = gates.chunk(2,1)
+        output, (hidden, cell) = self.rnn(embedded, (hidden, cell))
 
-        reset_gate = F.sigmoid(reset_gate)
-        update_gate = F.sigmoid(update_gate)
+        #output = [sent len, batch size, hid dim * n directions]
+        #hidden = [n layers * n directions, batch size, hid dim]
+        #cell = [n layers * n directions, batch size, hid dim]
 
-        prev_s_tilde = self.input_in(trg_word) + self.hidden_in(prev_s) + self.ctx_in(ctx)
-        prev_s_tilde = F.tanh(prev_s_tilde)
+        #sent len and n directions will always be 1 in the decoder, therefore:
+        #output = [1, batch size, hid dim]
+        #hidden = [n layers, batch size, hid dim]
+        #cell = [n layers, batch size, hid dim]
 
-        prev_s = torch.mul((1-reset_gate), prev_s) + torch.mul(reset_gate, prev_s_tilde)
-        return prev_s
-        
+        # prediction = self.out(output.squeeze(0))
+        prediction = self.adaptive_softmax.log_prob(output.squeeze(0))
+        #prediction = [batch size, output dim]
+
+        return prediction, hidden, cell

model/Encoder.py
@@ -6,31 +6,43 @@ from utils import *
 
 
 class Encoder(nn.Module):
-    def __init__(self, vocab_size, embed_dim, hidden_dim, num_layers=2): 
-        super(Encoder, self).__init__()
-        self.num_layers = num_layers
-        self.hidden_dim = hidden_dim
-        
-        self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.gru = nn.GRU(embed_dim, self.hidden_dim, self.num_layers, batch_first=True, bidirectional=True, )
-        
-    def forward(self, source, src_length=None, hidden=None):
-        '''
-        source: B x T 
-        '''
-        batch_size = source.size(0)
-        src_embed = self.embedding(source)
-        
-        if hidden is None:
-            h_size = (self.num_layers *2, batch_size, self.hidden_dim)
-            enc_h_0 = Variable(src_embed.data.new(*h_size).zero_(), requires_grad=False)
+    def __init__(self, input_dim, emb_dim, hid_dim, n_layers, dropout=0.5, no_langs=3):
+        super().__init__()
 
-        if src_length is not None:
-            src_embed = nn.utils.rnn.pack_padded_sequence(src_embed, src_length, batch_first=True)
+        self.input_dim = input_dim
+        self.emb_dim = emb_dim
+        self.hid_dim = hid_dim
+        self.n_layers = n_layers
+        self.dropout = dropout
+        self.M = nn.Parameter(torch.randn(hid_dim, hid_dim//2))
+        self.N = nn.Parameter(torch.randn(no_langs, hid_dim, hid_dim//2))
 
-        enc_h, enc_h_t = self.gru(src_embed, enc_h_0) 
+        self.embedding = nn.Embedding(input_dim, emb_dim)
 
-        if src_length is not None:
-            enc_h, _ = nn.utils.rnn.pad_packed_sequence(enc_h, batch_first=True)
+        self.rnn = nn.LSTM(emb_dim, hid_dim, n_layers, dropout = dropout)
 
-        return enc_h, enc_h_t
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, src, src_lang):
+
+        #src = [src sent len, batch size]
+
+        embedded = self.dropout(self.embedding(src))
+
+        #embedded = [src sent len, batch size, emb dim]
+
+        outputs, (hidden, cell) = self.rnn(embedded)
+
+        sem_emb = torch.mm(hidden.squeeze(0), self.M)
+        # print(self.N[src_lang, :, :].size())
+        syn_emb = torch.bmm(hidden.squeeze(0).unsqueeze(1), self.N[src_lang, :, :]).squeeze(1)
+
+        hidden = torch.cat((sem_emb, syn_emb), dim=1).unsqueeze(0)
+
+        #outputs = [src sent len, batch size, hid dim * n directions]
+        #hidden = [n layers * n directions, batch size, hid dim]
+        #cell = [n layers * n directions, batch size, hid dim]
+
+        #outputs are always from the top hidden layer
+
+        return hidden, cell

model/Seq2Seq.py
@@ -1,32 +1,53 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np 
+import numpy as np
 
 from utils import *
 from model import *
 
 class Seq2Seq(nn.Module):
-    def __init__(self, src_nword, trg_nword, num_layer, embed_dim, hidden_dim, max_len, trg_soi):
-        super(Seq2Seq, self).__init__()
+    def __init__(self, encoder, decoder, device, nout, cutoffs=[1000, 10000]):
+        super().__init__()
 
-        self.hidden_dim = hidden_dim
-        self.trg_nword = trg_nword
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
 
-        self.encoder = Encoder(src_nword, embed_dim, hidden_dim)
-        self.linear = nn.Linear(hidden_dim, hidden_dim)
-        self.decoder = Decoder(trg_nword, embed_dim, hidden_dim, max_len, trg_soi)
 
-    
-    def forward(self, source, src_length=None, target=None):
-        batch_size = source.size(0)
-        
-        enc_h, enc_h_t = self.encoder(source, src_length) # B x S x 2*H / 2 x B x H 
-        
-        dec_h0 = enc_h_t[-1] # B x H 
-        dec_h0 = F.tanh(self.linear(dec_h0)) # B x 1 x 2*H
+        # self.adaptive_softmax = nn.AdaptiveLogSoftmaxWithLoss(nout, nout, cutoffs=cutoffs)
 
-        out = self.decoder(enc_h, dec_h0, target) # B x S x H
-        out = F.log_softmax(out.contiguous().view(-1, self.trg_nword))
+        assert encoder.hid_dim == decoder.hid_dim, \
+            "Hidden dimensions of encoder and decoder must be equal!"
+        assert encoder.n_layers == decoder.n_layers, \
+            "Encoder and decoder must have equal number of layers!"
 
-        return out
+    def forward(self, src, trg, lng, teacher_forcing_ratio = 0.5):
+
+        #src = [src sent len, batch size]
+        #trg = [trg sent len, batch size]
+        #teacher_forcing_ratio is probability to use teacher forcing
+        #e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
+
+        batch_size = trg.shape[1]
+        max_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+
+        #tensor to store decoder outputs
+        outputs = torch.zeros(max_len, batch_size, trg_vocab_size).to(self.device)
+
+        #last hidden state of the encoder is used as the initial hidden state of the decoder
+        hidden, cell = self.encoder(src, lng)
+
+        #first input to the decoder is the <sos> tokens
+        input = trg[0,:]
+
+        for t in range(1, max_len):
+
+            output, hidden, cell = self.decoder(input, hidden, cell)
+            outputs[t] = output
+            teacher_force = random.random() < teacher_forcing_ratio
+            top1 = output.max(1)[1]
+            input = (trg[t] if teacher_force else top1)
+
+        return outputs

trainer.py
@@ -13,26 +13,33 @@ import os
 from logger import Logger
 from tqdm import tqdm
 
-from prepro import *
+from data import *
 from utils import *
 from model.Seq2Seq import Seq2Seq
+from model.Encoder import Encoder
+from model.Decoder import Decoder
 from bleu import *
 
 
 class Trainer(object):
-    def __init__(self, train_loader, val_loader, vocabs, args):
+    def __init__(self, dp, args):
 
         # Language setting
         self.max_len = args.max_len
+        self.args = args
 
         # Data Loader
-        self.train_loader = train_loader
-        self.val_loader = val_loader
+        self.dp = dp
+        if torch.cuda.is_available():
+            if not args.cuda:
+                print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+        self.device = torch.device("cuda:" + str(args.gpu) if args.cuda else "cpu")
 
         # Path
         self.data_path = args.data_path
         self.sample_path = os.path.join('./samples/' + args.sample)
-        self.log_path = os.path.join('./logs/' + args.log) 
+        self.log_path = os.path.join('./logs/' + args.log)
 
         if not os.path.exists(self.sample_path): os.makedirs(self.sample_path)
         if not os.path.exists(self.log_path): os.makedirs(self.log_path)
@@ -47,36 +54,43 @@ class Trainer(object):
         # Training setting
         self.batch_size = args.batch_size
         self.num_epoch = args.num_epoch
-        self.iter_per_epoch = len(train_loader)
+        # self.iter_per_epoch = len(train_loader)
 
         # Log
         self.logger = open(self.log_path+'/log.txt','w')
         self.sample = open(self.sample_path+'/sample.txt','w')
         self.tf_log = Logger(self.log_path)
 
-        self.build_model(vocabs)
+        self.build_model(self.dp.vocab)
+
+    def pad_sequences(self, s):
+        pad_token = self.dp.vocab.stoi['<PAD>']
+        # print(s)
+        lengths = [len(s1) for s1 in s]
+        longest_sent = max(lengths)
+        padded_X = np.ones((self.args.batch_size, longest_sent), dtype=np.int64) * pad_token
+        for i, x_len in enumerate(lengths):
+            sequence = s[i]
+            padded_X[i, 0:x_len] = sequence[:x_len]
+        # print(padded_X)
+        return padded_X
 
 
     def build_model(self, vocabs):
         # build dictionaries
-        self.src_vocab = vocabs['src_vocab']
-        self.trg_vocab = vocabs['trg_vocab']
-        self.src_inv_vocab = vocabs['src_inv_vocab']
-        self.trg_inv_vocab = vocabs['trg_inv_vocab']
-        self.trg_soi = self.trg_vocab[SOS_WORD]
+        self.vocab = self.dp.vocab
+        # print(len(self.vocab.itos))
+        self.encoder = Encoder(len(self.vocab.itos), self.embed_dim, self.hidden_dim, self.num_layer)
+        self.decoder = Decoder(len(self.vocab.itos), self.embed_dim, self.hidden_dim, self.num_layer)
 
-        self.src_nword = len(self.src_vocab)
-        self.trg_nword = len(self.trg_vocab)
-        
         # build the model
-        self.model = Seq2Seq(self.src_nword, self.trg_nword, self.num_layer, self.embed_dim, self.hidden_dim, 
-        					  self.max_len, self.trg_soi)
+        self.model = Seq2Seq(self.encoder, self.decoder, self.device, len(self.vocab.itos)).to(self.device)
 
         # set the criterion and optimizer
-        self.criterion = nn.NLLLoss()
+        self.criterion = nn.NLLLoss(ignore_index=self.dp.vocab.stoi['<PAD>'])
         self.optimizer = optim.Adam(self.model.parameters(), lr=self.lr)
         self.scheduler = optim.lr_scheduler.StepLR(self.optimizer, 1, gamma=0.8)
-        
+
         if torch.cuda.is_available():
             self.model.cuda()
 
@@ -84,129 +98,133 @@ class Trainer(object):
         print (self.criterion)
         print (self.optimizer)
 
+    def count_parameters(self, model):
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+    def evaluate(self):
+        self.model.eval()
+        start_time = time.time()
+        epoch_loss = 0
+        num_batches = len(self.dp.val_src)//self.args.batch_size
+        epoch_bleu = 0
+        tgt = []
+        pred = []
+        with torch.no_grad():
+            for i in range(num_batches):
+                src = self.pad_sequences(self.dp.val_src[i*self.args.batch_size : (i+1)*self.args.batch_size])
+                tgt = self.pad_sequences(self.dp.val_tgt[i*self.args.batch_size : (i+1)*self.args.batch_size])
+                lng = self.dp.val_srclng[i*self.args.batch_size : (i+1)*self.args.batch_size]
+
+                src = torch.LongTensor(src).to(self.device).transpose(0, 1)
+                tgt = torch.LongTensor(tgt).to(self.device).transpose(0, 1)
+                lng = torch.LongTensor(lng).to(self.device)
+
+                output = self.model(src, tgt, lng)
+                # print(output.size())
+                output_ = output[1:].view(-1, output.shape[-1])
+                # print(tgt[1:, :].size())
+                tgt_ = (tgt[1:, :]).contiguous().view(-1)
+                loss = self.criterion(output_, tgt_)
+                epoch_loss += loss.item()
+
+                pred_sents = []
+                trg_sents = []
+                output = output.transpose(0, 1)
+                tgt = tgt.transpose(0, 1)
+
+                for j in range(self.args.batch_size):
+                    # print(output.size())
+                    pred_sent = self.get_sentence(output[j, :, :].data.cpu().numpy().argmax(axis=-1).tolist(), 'tgt')
+                    trg_sent = self.get_sentence(tgt[j].data.cpu().numpy().tolist(), 'tgt')
+                    pred_sents.append(pred_sent)
+                    trg_sents.append(trg_sent)
+                epoch_bleu += get_bleu(pred_sents, trg_sents)
+            message = "Val loss: %1.3f  val_bleu:  %1.3f , val_ppl: %4.3f elapsed: %1.3f " % (
+            epoch_loss/num_batches, epoch_bleu/num_batches, np.exp(epoch_loss/num_batches), time.time() - start_time)
+            print(message)
+
+        return epoch_bleu/num_batches
 
     def train(self):
         self.best_bleu = .0
-        
-        for epoch in range(self.num_epoch):
+        patience = 0
+        print(f'The model has {self.count_parameters(self.model):,} trainable parameters')
+
+        for epoch in range(1, self.num_epoch):
             #self.scheduler.step()
-            self.train_loss = AverageMeter()
-            self.train_bleu = AverageMeter()
+            self.train_loss = 0
+            self.train_bleu = 0
             start_time = time.time()
 
-            for i, batch in enumerate(tqdm(self.train_loader)):
+            num_batches = len(self.dp.train_src)//self.args.batch_size
+
+            for i in tqdm(range(num_batches)):
                 self.model.train()
 
-                src_input = batch.src[0]; src_length = batch.src[1]
-                trg_input = batch.trg[0][:,:-1]; trg_output=batch.trg[0][:,1:]; trg_length = batch.trg[1]
-                batch_size, trg_len = trg_input.size(0), trg_input.size(1)
+                src = self.pad_sequences(self.dp.train_src[i*self.args.batch_size : (i+1)*self.args.batch_size])
+                tgt = self.pad_sequences(self.dp.train_tgt[i*self.args.batch_size : (i+1)*self.args.batch_size])
+                lng = self.dp.train_srclng[i*self.args.batch_size : (i+1)*self.args.batch_size]
 
-                decoder_logit = self.model(src_input, src_length.tolist(), trg_input)
-                pred = decoder_logit.view(batch_size, trg_len, -1)
+                src = torch.LongTensor(src).to(self.device).transpose(0, 1)
+                tgt = torch.LongTensor(tgt).to(self.device).transpose(0, 1)
+                lng = torch.LongTensor(lng).to(self.device)
+                # print(src.size())
+                # print(tgt.size())
+
+                output = self.model(src, tgt, lng)
+                # print(output.size())
+                output_ = output[1:].view(-1, output.shape[-1])
+                # print(tgt[1:, :].size())
+                tgt_ = (tgt[1:, :]).contiguous().view(-1)
 
                 self.optimizer.zero_grad()
-                loss = self.criterion(decoder_logit, trg_output.contiguous().view(-1))
+                loss = self.criterion(output_, tgt_)
                 loss.backward()
-                torch.nn.utils.clip_grad_norm(self.model.parameters(), self.grad_clip)
-                self.optimizer.step()
+                torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.grad_clip)
 
-                # Compute BLEU score and Loss
+                self.optimizer.step()
+                self.train_loss += loss.item()
+
                 pred_sents = []
                 trg_sents = []
-                for j in range(batch_size):
-                    pred_sent = self.get_sentence(tensor2np(pred[j]).argmax(axis=-1), 'trg')
-                    trg_sent = self.get_sentence(tensor2np(trg_output[j]), 'trg')
+                output = output.transpose(0, 1)
+                tgt = tgt.transpose(0, 1)
+
+                for j in range(self.args.batch_size):
+                    pred_sent = self.get_sentence(output[j].data.cpu().numpy().argmax(axis=-1).tolist(), 'tgt')
+                    trg_sent = self.get_sentence(tgt[j].data.cpu().numpy().tolist(), 'tgt')
                     pred_sents.append(pred_sent)
                     trg_sents.append(trg_sent)
                 bleu_value = get_bleu(pred_sents, trg_sents)
-                self.train_bleu.update(bleu_value, 1)
-                self.train_loss.update(loss.data[0], batch_size)
+                self.train_bleu += bleu_value
 
-                if i % 5000 == 0 and i != 0:
-                    self.print_train_result(epoch, i, start_time)
-                    self.print_sample(batch_size, epoch, i, src_input, trg_output, pred)
-                    self.eval(epoch, i)
-                    self.train_loss = AverageMeter()
-                    self.train_bleu = AverageMeter()
+                if i%self.args.log_interval == 0 and i>0:
+                    message = "Train epoch: %d  iter: %d  train loss: %1.3f  train_bleu:  %1.3f , train_ppl: %4.3f elapsed: %1.3f " % (
+                    epoch, i, self.train_loss/self.args.log_interval, self.train_bleu/self.args.log_interval, np.exp(self.train_loss/self.args.log_interval), time.time() - start_time)
+                    print(message)
+                    self.train_loss = 0
+                    self.train_bleu = 0
                     start_time = time.time()
 
-                    # Logging tensorboard
-                    info = {
-                        'epoch': epoch,
-                        'train_iter': i,
-                        'train_loss': self.train_loss.avg,
-                        'train_bleu': self.train_bleu.avg               
-                        }
-                    for tag, value in info.items():
-                        self.tf_log.scalar_summary(tag, value, (epoch * self.iter_per_epoch)+i+1)
-
-            self.print_train_result(epoch, i, start_time)
-            self.print_sample(batch_size, epoch, i, src_input, trg_output, pred)           
-            self.eval(epoch, i)                
-
-
-    def eval(self, epoch, train_iter):
-        self.model.eval()
-        val_bleu = AverageMeter()
-        start_time = time.time()
-        
-        for i, batch in enumerate(tqdm(self.val_loader)):
-            src_input = batch.src[0]; src_length = batch.src[1]
-            trg_input = batch.trg[0][:,:-1]; trg_output=batch.trg[0][:,1:]; trg_length = batch.trg[1]
-            batch_size, trg_len = trg_input.size(0), trg_input.size(1)
-
-            decoder_logit = self.model(src_input, src_length.tolist())
-            pred = decoder_logit.view(batch_size, self.max_len, -1)
-
-            # Compute BLEU score
-            pred_sents = []
-            trg_sents = []            
-            for j in range(batch_size):
-                pred_sent = self.get_sentence(tensor2np(pred[j]).argmax(axis=-1), 'trg')
-                trg_sent = self.get_sentence(tensor2np(trg_output[j]), 'trg')
-                pred_sents.append(pred_sent)
-                trg_sents.append(trg_sent)
-            bleu_value = get_bleu(pred_sents, trg_sents)
-            val_bleu.update(bleu_value, 1)
-               
-        self.print_valid_result(epoch, train_iter, val_bleu.avg, start_time)       
-        self.print_sample(batch_size, epoch, train_iter, src_input, trg_output, pred)
-
-        # Save model if bleu score is higher than the best 
-        if self.best_bleu < val_bleu.avg:
-            self.best_bleu = val_bleu.avg        
-            checkpoint = {
-                    'model': self.model,
-                    'epoch': epoch
-                }
-            torch.save(checkpoint, self.log_path+'/Model_e%d_i%d_%.3f.pt' % (epoch, train_iter, val_bleu.avg))                 
-
-        # Logging tensorboard
-        info = {
-            'epoch': epoch,
-            'train_iter': train_iter,
-            'train_loss': self.train_loss.avg,
-            'train_bleu': self.train_bleu.avg,
-            'bleu': val_bleu.avg                
-            }
-
-        for tag, value in info.items():
-            self.tf_log.scalar_summary(tag, value, (epoch * self.iter_per_epoch)+train_iter+1)
-
+            val_bleu = self.evaluate()
+            if val_bleu > self.best_bleu:
+                self.best_bleu = val_bleu
+                torch.save(self.model, 'models/model.pb')
+                patience = 0
+            else:
+                patience +=1
+            if patience > 3:
+                break
 
     def get_sentence(self, sentence, side):
         def _eos_parsing(sentence):
-            if EOS_WORD in sentence:
-                return sentence[:sentence.index(EOS_WORD)+1]
+            if '<EOS>' in sentence:
+                return sentence[:sentence.index('<EOS>')+1]
             else:
                 return sentence
 
-        # index sentence to word sentence                      
-        if side == 'trg':
-            sentence = [self.trg_inv_vocab[x] for x in sentence]
-        else:
-            sentence = [self.src_inv_vocab[x] for x in sentence]
+        # index sentence to word sentence
+        sentence = [self.dp.vocab.itos[s] for s in sentence]
 
         return _eos_parsing(sentence)
 
@@ -216,11 +234,11 @@ class Trainer(object):
         print (mode, '\n')
         self.logger.write(mode+'\n')
         self.sample.write(mode+'\n')
-        
+
         message = "Train epoch: %d  iter: %d  train loss: %1.3f  train bleu: %1.3f  elapsed: %1.3f " % (
         epoch, train_iter, self.train_loss.avg, self.train_bleu.avg, time.time() - start_time)
         print (message, '\n\n')
-        self.logger.write(message+'\n\n')   
+        self.logger.write(message+'\n\n')
 
 
     def print_valid_result(self, epoch, train_iter, val_bleu, start_time):
@@ -233,7 +251,7 @@ class Trainer(object):
         epoch, train_iter, self.train_loss.avg, self.train_bleu.avg, val_bleu, time.time() - start_time)
         print (message, '\n\n' )
         self.logger.write(message+'\n\n')
-        
+
 
     def print_sample(self, batch_size, epoch, train_iter, source, target, pred):
 